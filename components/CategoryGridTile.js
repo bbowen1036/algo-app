367ea4380f8f7f2028ee3bf4a738e58f1478b306
brianbowen <bbowen1036@gmail.com>
2021-02-23 18:04:51 -0800

    category seed data

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -14,17 +14,18 @@ const CategoryGridTile = (props) => {
   if (Platform.OS === "android" && Platform.Version >= 21)
     TouchableCmp = TouchableNativeFeedback;
 
-
   return (
-    <TouchableCmp style={styles.gridItem} onPress={props.onSelect}>
-      <View
-        style={{ ...styles.container, ...{ backgroundColor: props.color } }}
-      >
-        <Text style={styles.title} numberOfLines={2}>
-          {props.title}
-        </Text>
-      </View>
-    </TouchableCmp>
+    <View style={styles.gridItem}>
+      <TouchableCmp style={{flex: 1}}  onPress={props.onSelect}>
+        <View
+          style={{ ...styles.container, ...{ backgroundColor: props.color } }}
+        >
+          <Text style={styles.title} numberOfLines={2}>
+            {props.title}
+          </Text>
+        </View>
+      </TouchableCmp>
+    </View>
   );
 };
 
@@ -33,6 +34,8 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 15,
     height: 150,
+    borderRadius: 10,
+    overflow: "hidden"
   },
   container: {
     flex: 1,
