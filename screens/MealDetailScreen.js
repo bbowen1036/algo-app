fddd87c7982601a19039937b9fdbdef628236666
brianbowen <bbowen1036@gmail.com>
2021-02-26 11:08:28 -0800

    passing data thru params

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -18,8 +18,11 @@ const ListItem = (props) => {
 };
 
 const MealDetailScreen = (props) => {
-  const availableMeals = useSelector(state => state.meals.meals);
+  const availableMeals = useSelector((state) => state.meals.meals);
   const mealId = props.navigation.getParam("mealId");
+  const currentMealIsFavorite = useSelector((state) =>
+    state.meals.favoriteMeals.some((meal) => meal.id === mealId)
+  );
 
   const selectedMeal = availableMeals.find((meal) => meal.id === mealId);
 
@@ -31,8 +34,13 @@ const MealDetailScreen = (props) => {
 
   useEffect(() => {
     // props.navigation.setParams({mealTitle: selectedMeal.title})
-    props.navigation.setParams({toggleFav: toggleFavoriteHandler});
+    props.navigation.setParams({ toggleFav: toggleFavoriteHandler });
   }, [toggleFavoriteHandler]);
+
+
+  useEffect(() => {
+    props.navigation.setParams({ isFav: currentMealIsFavorite})
+  }, [currentMealIsFavorite]);
 
   return (
     <ScrollView>
@@ -43,11 +51,11 @@ const MealDetailScreen = (props) => {
         <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
       </View>
       <Text style={styles.title}>Ingredients</Text>
-      {selectedMeal.ingredients.map(ingredient => (
+      {selectedMeal.ingredients.map((ingredient) => (
         <ListItem key={ingredient}>{ingredient}</ListItem>
       ))}
       <Text style={styles.title}>steps</Text>
-      {selectedMeal.steps.map(step => (
+      {selectedMeal.steps.map((step) => (
         <ListItem key={step}>{step}</ListItem>
       ))}
     </ScrollView>
@@ -59,15 +67,16 @@ const MealDetailScreen = (props) => {
 MealDetailScreen.navigationOptions = navigationData => {
   // const mealId = navigationData.navigation.getParam('mealId');
   const mealTitle = navigationData.navigation.getParam("mealTitle");
-  const toggleFavorite = navigationData.navigation.getParam("toggleFav")
+  const toggleFavorite = navigationData.navigation.getParam("toggleFav");
+  const isFavorite = navigationData.navigation.getParam("isFav");
   // const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return {
     headerTitle: mealTitle,
     headerRight: () => (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
         <Item
-          title="Favorite"
-          iconName="ios-star"
+          title={ isFavorite ? "👎🏾" : "👍🏾" }
+          // iconName="ios-star"
           onPress={toggleFavorite}
         />
       </HeaderButtons>
