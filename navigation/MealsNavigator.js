91038162569b7b3cef513fbc359b74fd39839a35
brianbowen <bbowen1036@gmail.com>
2021-02-25 16:22:53 -0800

    drawer navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,8 +1,9 @@
 import React from "react"
 import { Platform } from "react-native";
-import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import { Ionicons } from "@expo/vector-icons";
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs"
 
@@ -11,6 +12,7 @@ import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealScreen from "../screens/CategoryMealScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 import FavoritesScreen from "../screens/FavoritesScreen";
+import FiltersScreen from "../screens/FiltersScreen";
 import Colors from "../constants/Colors";
 
 const defaultStackNavOptions = {
@@ -46,7 +48,7 @@ const FavNavigator = createStackNavigator({
     defaultNavigationOptions: defaultStackNavOptions
   });
 
-  
+
 const tabScreenConfig = {
   Meals: {
     screen: MealsNavigator,
@@ -79,5 +81,14 @@ const MealsFavTabNavigator =
         }
       );
 
+const FiltersNavigator = createStackNavigator({
+     Filters: FiltersScreen
+});
+
+const MainNavigator = createDrawerNavigator({
+  MealsFavs: MealsFavTabNavigator,
+  Filters: FiltersNavigator   
+});    
+
 // createAppContainer is the pattern to wrap root using navigation
-export default createAppContainer(MealsFavTabNavigator);
+export default createAppContainer(MainNavigator);
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -8,10 +8,12 @@ import {
   FlatList,
   TouchableOpacity,
 } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { CATEGORIES } from "../data/dummy-data";
 // Components 
 import CategoryGridTile from "../components/CategoryGridTile";
+import HeaderButton from "../components/HeaderButton";
 
 const CategoriesScreen = (props) => {
   const renderGridItem = (itemData) => {
@@ -41,9 +43,18 @@ const CategoriesScreen = (props) => {
   );
 };
 
-CategoriesScreen.navigationOptions = {
-  headerTitle: "Meal Categories",
-}
+CategoriesScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: "Meal Categories",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title="Menu" /*iconName="ios-menu"*/ onPress={() => {
+          navData.navigation.toggleDrawer();
+        }} />
+      </HeaderButtons>
+    ),
+  }
+};
 
 const styles = StyleSheet.create({
   screen: {
